d9cdfb6021ed26c78453f0b493aefdc6d77d61ec
AliALmasiZ <Ali1385Almasi@gmail.com>
2025-05-03 10:57:29 +0330

    some little changes

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -117,4 +117,4 @@ public class Game {
     public boolean isMapVisible(){
         return mapVisible;
     }
-}
+    }
--- src/main/java/models/player/Skill.java
+++ src/main/java/models/player/Skill.java
@@ -4,8 +4,8 @@ public class Skill {
     protected int experience;
     protected int level;
 
-    public void addExperience() {
-
+    public void addExperience(int experience) {
+        this.experience += experience;
     }
 
     private void addLevel() {
