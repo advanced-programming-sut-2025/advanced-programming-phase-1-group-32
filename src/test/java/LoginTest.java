e365bbd819bf9bc74ea6640d62c1a22c02c88124
AliAlmasiZ <Ali1385Almasi@gmail.com>
2025-05-16 20:48:55 +0330

    added some login tests

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,2 +1,89 @@
+import models.Account;
+import models.App;
+import models.enums.Gender;
+import models.enums.Menu;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import views.LoginMenu;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class LoginTest {
+
+    private Account account1 = new Account(Gender.MALE, "test@gmail.com", "AliAlm", "TestPass#403","AliAlmasi");
+    private Account account2 = new Account(Gender.MALE, "test@gmail.com", "AliAlm", "TestPass#403","AliAlmasi2");
+    private LoginMenu menu;
+    private final PrintStream originalOut = System.out;
+    private final InputStream originalIn = System.in;
+    private ByteArrayOutputStream out;
+    private Scanner scanner;
+
+    @BeforeEach
+    void setup() {
+        App.setLoggedInAccount(null);
+        App.addAccount(account1);
+        App.addAccount(account2);
+        App.setCurrentMenu(Menu.LOGIN_MENU);
+        menu = new LoginMenu();
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
+
+    @AfterEach
+    void restoreStreams() {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+    }
+
+    private void setIn(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+    private void checkStartExpected(String startExpected) {
+        scanner = new Scanner(System.in);
+        App.getCurrentMenu().checker(scanner);
+        String output = out.toString().trim();
+        assertTrue(output.startsWith(startExpected), "your code output:\n" + output + "\nexpected:\n" + startExpected + "\n");
+    }
+
+    private void checkExpected(String expected) {
+        scanner = new Scanner(System.in);
+        App.getCurrentMenu().checker(scanner);
+        String output = out.toString().trim();
+        assertEquals(expected, output, "your code output:\n" + output + "\nexpected:\n" + expected + "\n");
+    }
+
+
+    @Test
+    void invalidUserName() {
+        setIn("login -u     meow -p asfasfasf");
+        checkExpected("username doesnt exist");
+    }
+
+    @Test
+    void invalidPass() {
+        setIn("login -u    AliAlmasi    -p    TestPass#40");
+        checkExpected("incorrect password");
+    }
+
+    @Test
+    void loginSuccessfully() {
+        setIn("    login    -u   AliAlmasi    -p    TestPass#403      ");
+        checkExpected("logged in successfully");
+    }
+
+    @Test
+
+
+
+
+
+
 }
--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -56,7 +56,7 @@ public class RegisterTest {
     @ParameterizedTest
     @CsvSource({
             "register -u Ali-Almasi -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gmail.com -g male",
-            "register -u AliAlmasi2 -p CorP@ss1 CorP@ss1 -n AliAlm -e Ali@gmail.com -g male",
+            "register -u AliAlmasi22 -p CorP@ss1 CorP@ss1 -n AliAlm -e Ali@gmail.com -g male",
             "register -u AliAlmasi3 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gm.ail.com -g male",
             "register -u AliAlmasi4 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Al.i@gmail.com -g male",
             "register -u AliAlmasi5 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gmail.com -g female"
@@ -65,10 +65,10 @@ public class RegisterTest {
     void validInput(String input) {
         String startExpected = "Account registered successfully!";
         setIn(input);
-        scanner = new Scanner(System.in);
-        menu.checker(scanner);
-        String output = out.toString().trim();
-        assertTrue(output.startsWith(startExpected), "your code output:\n" + output + "\nexpected:\n" + startExpected + "\n");
+            scanner = new Scanner(System.in);
+            App.getCurrentMenu().checker(scanner);
+            String output = out.toString().trim();
+            assertTrue(output.startsWith(startExpected), "your code output:\n" + output + "\nexpected:\n" + startExpected + "\n");
 
     }
 
@@ -80,7 +80,7 @@ public class RegisterTest {
     void usernameValidation(String input) {
         setIn(input);
         scanner = new Scanner(System.in);
-        menu.checker(scanner);
+        App.getCurrentMenu().checker(scanner);
         String output = out.toString().trim();
         String startExpected = "Username contains invalid characters";//TODO
         assertTrue(output.startsWith(startExpected), "your code output:\n" + output + "\nexpected:\n" + startExpected + "\n");
@@ -92,7 +92,7 @@ public class RegisterTest {
         setIn(makeRegisterCommand("AliAlm", "CorrectP@ss1", "CorrectP@ss1",
                 "AliAlm", "Ali@gmail.com", "male"));
         scanner = new Scanner(System.in);
-        menu.checker(scanner);
+        App.getCurrentMenu().checker(scanner);
         String output = out.toString().trim();
         String startExpected = "you should choose a new username, do you want to continue withthis username?";
         assertTrue(output.startsWith(startExpected), "your code output:\n" + output + "\nexpected:\n" + startExpected + "\n");
@@ -103,7 +103,12 @@ public class RegisterTest {
     @CsvSource({
             "register -u AliAlm1 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gmail.co.m -g male, \"m\" is too short",
             "register -u AliAlm2 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gm..ail.com -g male, email should not contain \"..\"",
+            "register -u AliAlm2 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gmail..com -g male, email should not contain \"..\"",
+            "register -u AliAlm2 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Al..i@gmail.com -g male, email should not contain \"..\"",
+            "register -u AliAlm2 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gmail......com -g male, email should not contain \"..\"",
             "register -u AliAlm3 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gma@il.com -g male, \"Ali@gma\" contains invalid characters",
+            "register -u AliAlm3 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gmail -g male, email format is invalid : sample@domain.com",
+            "register -u AliAlm3 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@.com -g male, email format is invalid : sample@domain.com",
             "register -u AliAlm4 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gmAil.com -g male, Account registered successfully!",
             "register -u AliAlm5 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gail.om -g male, Account registered successfully!",
             "register -u AliAlm6 -p CorrectP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gl.com -g male, Account registered successfully!"
@@ -111,7 +116,25 @@ public class RegisterTest {
     void emailValidation(String input, String startExpected) {
         setIn(input);
         scanner = new Scanner(System.in);
-        menu.checker(scanner);
+        App.getCurrentMenu().checker(scanner);
+        String output = out.toString().trim();
+        assertTrue(output.startsWith(startExpected), "your code output:\n" + output + "\nexpected:\n" + startExpected + "\n");
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "register -u AliAlm -p CorrectP@ss1 CorectP@ss1 -n AliAlm -e Ali@gmail.com -g male, Passwords do not match",
+            "register -u AliAlm -p CorrectPss1 CorrectPss1 -n AliAlm -e Ali@gmail.com -g male, Password must contain at least one special character",
+            "register -u AliAlm -p correctP@ss1 CorrectP@ss1 -n AliAlm -e Ali@gmail.com -g male, Passwords do not match",
+            "register -u AliAlm -p correctp@ss1 correctp@ss1 -n AliAlm -e Ali@gmail.com -g male, Password must contain at least one uppercase letter",
+            "register -u AliAlm -p Correc..tP@ss1 Correc..tP@ss1 -n AliAlm -e Ali@gmail.com -g male, password contains invalid characters",
+
+    })
+    void passwordValidation(String input, String startExpected) {
+        setIn(input);
+        scanner = new Scanner(System.in);
+        App.getCurrentMenu().checker(scanner);
         String output = out.toString().trim();
         assertTrue(output.startsWith(startExpected), "your code output:\n" + output + "\nexpected:\n" + startExpected + "\n");
     }
