057158f11f5082268410326d79721d709780bb30
Parsa <parsazamirikisomi@gmail.com>
2025-05-18 01:11:59 +0330

    Serialization3

--- src/main/java/models/entities/EntityList.java
+++ src/main/java/models/entities/EntityList.java
@@ -2,13 +2,29 @@ package models.entities;
 
 import models.entities.components.EntityComponent;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-public class EntityList extends ArrayList<Entity> implements EntityObserver, Serializable {
-    private final Map<Class<? extends EntityComponent>, ArrayList<EntityComponent>> components = new HashMap<>();
+public class EntityList extends ArrayList<Entity> implements EntityObserver, Serializable{
+    private Map<Class<? extends EntityComponent>, ArrayList<EntityComponent>> components = new HashMap<>();
+
+    public EntityList(Collection<? extends Entity> c) {
+        super(c);
+    }
+
+    public EntityList() {
+        super();
+    }
+
+    public EntityList(int initialCapacity) {
+        super(initialCapacity);
+    }
 
     @Override
     public boolean add(Entity entity) {
@@ -50,4 +66,31 @@ public class EntityList extends ArrayList<Entity> implements EntityObserver, Ser
         components.putIfAbsent(clazz, new ArrayList<>());
         return (ArrayList<T>) components.get(clazz);
     }
+
+    @Serial
+    @SuppressWarnings("unchecked")
+    private void readObject(ObjectInputStream in)
+            throws IOException, ClassNotFoundException {
+        // First, read in your own fields (this populates 'components')
+        in.defaultReadObject();
+
+        // Now rebuild the list itself
+        int size = in.readInt();
+        for (int i = 0; i < size; i++) {
+            Entity e = (Entity) in.readObject();
+
+            // Use your overridden add() to:
+            //  1) add it into the ArrayList
+            //  2) re‐attach your EntityObserver
+            //  3) re‐populate your components map
+            super.add(e);
+            e.addObserver(this);
+            for (EntityComponent c : e.getComponents()) {
+                components
+                        .computeIfAbsent(c.getClass(), k -> new ArrayList<>())
+                        .add(c);
+            }
+        }
+    }
 }
+
